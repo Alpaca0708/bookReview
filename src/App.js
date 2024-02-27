d84fe42b31ce8110f2cc4d2ab3e553429f9fe8e8
Ellie <camera20120109@gmail.com>
2024-02-27 15:45:34 +0800

    nav bar

--- src/App.js
+++ src/App.js
@@ -2,57 +2,76 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 import Card from './components/Card'
+import
 
 function App() {  
 
   return (
     <div style={{display:'flex'}}>
-        <nav style={{display:'flex',flexDirection:'column', color:'#727272'}}>
-            <h1>
+        <div style={{display:'flex',flexDirection:'column', color:'#727272', border:'solid', height:'100%', width:'248px',alignItems:'flex-start'}}>           
+            <h2 >
               Ventur
-            </h1>
-            <hr style={{borderColor:'#727272'}}/>
-            <p>
-              Dashboard
-            </p>
-            <p>
-              Notes
-            </p>
-            <p>
-              Tasks
-            </p>
-            <p>
-              Calendar
-            </p>
-            <p>
-              Database
-            </p>
-            <p>
-              Analytics
-            </p>
-            <hr/>
-            <p>
-              Setting
-            </p>
-        </nav>
-        <main>
-            <div style={{display:'flex', }}>
-                <input placeholder='search'></input>
-                <p>Help Center</p>
+            </h2>            
+            <div style={{}}>
+                <div className='navStyle' style={{backgroundColor: 'bisque'}}>
+                    <p>
+                      Dashboard
+                    </p>
+                    <DownOutlined />
+                </div>
+                <div className='navStyle'>
+                    <p>
+                      Notes
+                    </p>
+                </div>
+                <div className='navStyle'>
+                    <p>
+                      Tasks
+                    </p>
+                </div>
+                <div className='navStyle'>
+                    <p>
+                      Calendar
+                    </p>
+                </div>
+                <div className='navStyle'>
+                    <p>
+                      Analytics
+                    </p>
+                </div>                             
+                <hr style={{}}/>
+                <p>
+                  Setting
+                </p>
+              </div>
+        </div>
+        <main style={{width:'100%'}}>
+            <div style={{display:'flex', border:'solid', justifyContent:'space-between', padding:'15px'}}>
+                <input placeholder='search' style={{}}></input>
+                <div style={{display:'flex'}}>
+                    <p>
+                      Help Center
+                    </p>
+                    <p>
+                      Ellie.L
+                    </p>
+                </div>  
             </div>
             <div style={{display:'flex', justifyContent:'space-between'}}>
                 <h3>
                   Notes
                 </h3>
-                <p>
-                  Sort By
-                </p>
-                <p>
-                  Filter
-                </p>
-                <p>
-                  Add Notes
-                </p>
+                <div>
+                    <button>
+                      Sort By
+                    </button>
+                    <button>
+                      Filter
+                    </button>
+                    <button>
+                      Add Notes
+                    </button>
+                </div>
             </div>
             <div></div>
         </main>
